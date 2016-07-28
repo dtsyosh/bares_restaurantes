03450747035cfe23d5b6f8d782d1770ada51cc57
yosh <yosh@yosh-Inspiron-5458>
2016-07-28 08:56:15 -0400

    Inicio do desenvolvimento de GenericDAO

---
+++ Bar_Restautante/src/dao/ConexaoBanco.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author yosh
+ */
+public class ConexaoBanco {
+     
+    public Connection getConnection() throws ClassNotFoundException{
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");           
+            return DriverManager.getConnection("jdbc:mysql://localhost/imobiliaria", "root", "3bjmf82,");
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
