b47bd37c4dd6f3c17d113abb0fdea4c690eb4e92
yosh <yosh@yosh-Inspiron-5458>
2016-08-04 08:20:20 -0400

    adicionado login

---
+++ Bar_Restautante_1/src/model/login.java
@@ -0,0 +1,49 @@
+package model;
+
+/**
+ *
+ * @author yosh
+ */
+public class login {
+    int id_login;
+    String login;
+    String senha;
+    int nivel;
+
+    public login() {
+    }
+
+    public int getId_login() {
+        return id_login;
+    }
+
+    public void setId_login(int id_login) {
+        this.id_login = id_login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+    
+    
+}
